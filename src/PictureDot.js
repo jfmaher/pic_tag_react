845bdf61fc33a797c7bdfea40f957d10a89853f0
James Maher <maher.j@live.com>
2018-10-03 15:05:59 +0100

    Added div for containing form that opens and closes onClick.

--- src/PictureDot.js
+++ src/PictureDot.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
 import './MovingDot.css'
+import './Form.css'
 
 class PictureDot extends Component{
   constructor(props) {
     super(props);
     this.state = {
+      form: {
+        open: false,
+        name: ''
+      },
       style: {
         position: 'absolute',
         top: props.y,
@@ -13,9 +18,24 @@ class PictureDot extends Component{
     };
   }
 
+  onClick = (ev) => {
+    this.setState((state, props) => {
+      return {form: {open: !this.state.form.open}}
+    })
+  };
+
   render() {
+    let form = null;
+    if (this.state.form.open){
+      form = (
+        <div className='form'>Form Div</div>
+      )
+    }
     return (
-      <div style={this.state.style} className='dot'>
+      <div style={this.state.style}
+           className='dot'
+           onClick={this.onClick}>
+        {form}
         Hello
       </div>
     )
