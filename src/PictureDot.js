6afe7761d64a20cd6c4b592544ce6e97bcf31dbf
j maher <jmaher@js-MacBook-Air.local>
2019-03-13 10:43:15 +0000

    Made initial colour selection form

--- src/PictureDot.js
+++ src/PictureDot.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import MovingDot from './MovingDot'
 import request from 'request'
 import './MovingDot.css'
@@ -12,6 +12,9 @@ class PictureDot extends MovingDot{
         position: 'absolute',
         top: props.y,
         left: props.x
+      },
+      dotStyle: {
+        backgroundColor: 'aquamarine'
       }
     };
     if(props.form)
@@ -49,6 +52,11 @@ class PictureDot extends MovingDot{
     console.log('sent dot'));
   }
 
+  changeColour = (ev) => {
+    const target = ev.target, value = ev.target.value;
+    this.setState({dotStyle:{...this.dotStyle, backgroundColor: value}})
+  }
+
   onClick = (ev) => {
     this.setState((state, props) => {
       return {form: {...state.form, display: !state.form.display}}
@@ -56,18 +64,40 @@ class PictureDot extends MovingDot{
   }
 
   render() {
+    const colours = ['red', 'yellow', 'aquamarine']
+    let colourRadios =[]
+    for (let index in colours){
+      const colour = colours[index];
+      colourRadios.push(
+        <div>
+          <input type='radio'
+                 name='colour'
+                 value={colour}
+                 onChange={this.changeColour}
+                 checked={this.state.dotStyle.backgroundColor == colour}/>
+                 <span>{colour}</span>
+
+        </div>
+      )
+    }
     return (
       <div style={this.state.style}>
         <div className='dot'
+             style={this.state.dotStyle}
              draggable
              onDragStart={(e) => this.onDragStart2(e, this.name)}
              onClick={this.onClick}>
           Hello
         </div>
         {this.state.form.display ?
-          <form className='form' onSubmit={this.onSubmit}>
-            <input key={this.name} name={this.name} value={this.state['form']['value']} onChange={this.onInput}/>
-          </form>
+          <Fragment>
+            <form className='form' onSubmit={this.onSubmit}>
+              <input key={this.name} name={this.name} value={this.state['form']['value']} onChange={this.onInput}/>
+            </form>
+            <div className='form radios'>
+              {colourRadios}
+            </div>
+          </Fragment>
         :''}
       </div>
     )
